36c27e13bcaac940a2737a9f2433034801470576
Juan Vargas <juandavc12@hotmail.com>
2022-01-23 18:28:20 -0500

    Solved exercises

--- 03-transforming-promises/answers.js
+++ 03-transforming-promises/answers.js
@@ -1,48 +1,75 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {*} promise 
- * @param {*} transformer 
+ *
+ * @param {*} promise
+ * @param {*} transformer
  * @returns {Promise}
  */
-function mapPromise(promise, transformer){
+function mapPromise(promise, transformer) {
   return new Promise((resolve, reject) => {
     /* IMPLEMENT ME!! */
+    promise
+      .then((value) => {
+        try {
+          resolve(transformer(value));
+        } catch (error) {
+          reject(error);
+        }
+      })
+      .catch((value) => {
+        try {
+          reject(transformer(value));
+        } catch (error) {
+          reject(error);
+        }
+      });
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromise(numberPromise){
-  return numberPromise
-    .then(/* IMPLEMENT ME! */);
+function squarePromise(numberPromise) {
+  return numberPromise.then((value) => {
+    const result = value * value;
+    if (!isNaN(result)) {
+      return result;
+    } else {
+      throw `Cannot convert '${value}' to a number!`;
+    }
+  });
 }
 
 /**
  * EXERCISE 3
- * 
- * @param {Promise<number | string>} numberPromise 
+ *
+ * @param {Promise<number | string>} numberPromise
  * @returns {Promise<number>}
  */
-function squarePromiseOrZero(promise){
-  return squarePromise(promise)
-    .catch(/* IMPLEMENT ME! */);
+function squarePromiseOrZero(promise) {
+  return squarePromise(promise).catch(() => 0);
 }
 
 /**
  * EXERCISE 4
- * 
- * @param {Promise} promise 
+ *
+ * @param {Promise} promise
  * @returns {Promise}
  */
-function switcheroo(promise){
-  return promise.then(/* IMPLEMENT ME */);
+function switcheroo(promise) {
+  return promise.then(
+    (value) => {
+      throw value;
+    },
+    (error) => {
+      return error;
+    }
+  );
 }
 
 /**
--- 04-chaining-promises/answers.js
+++ 04-chaining-promises/answers.js
@@ -1,38 +1,52 @@
 /**
- * 
+ *
  * EXERCISE 1
- * 
- * @param {Promise} promise 
- * @param {function} asyncTransformer 
+ *
+ * @param {Promise} promise
+ * @param {function} asyncTransformer
  */
-function flatMapPromise(promise, asyncTransformer){
+function flatMapPromise(promise, asyncTransformer) {
   return new Promise((resolve, reject) => {
     promise
-      .then(/* IMPLEMENT ME! */);
+      .then((value) => resolve(asyncTransformer(value)))
+      .catch((value) => reject(value));
   });
 }
 
 /**
- * 
+ *
  * EXERCISE 2
- * 
- * @param {Promise} firstPromise 
- * @param {function} slowAsyncProcess 
+ *
+ * @param {Promise} firstPromise
+ * @param {function} slowAsyncProcess
  */
-function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess){
-  return firstPromise.then(/* IMPLEMENT ME! */);
+function chainTwoAsyncProcesses(firstPromise, slowAsyncProcess) {
+  return firstPromise.then((value) => slowAsyncProcces(value));
 }
 
 /**
- * 
+ *
  * EXERCISE 3
- * 
- * @param {function} getUserById 
- * @param {function} getOrganizationById 
+ *
+ * @param {function} getUserById
+ * @param {function} getOrganizationById
  */
-function makeGetUserByIdWithOrganization(getUserById, getOrganizationById){
-  return function getUserByIdWithOrganization(userId){
-    /* IMPLEMENT ME! */
+function makeGetUserByIdWithOrganization(getUserById, getOrganizationById) {
+  return function getUserByIdWithOrganization(userId) {
+    return new Promise((resolve) => {
+      getUserById(userId).then((value) => {
+        if (value !== undefined) {
+          getOrganizationById(val['organizationId']).then((value2) => {
+            resolve({
+              ...value,
+              organization: value2,
+            });
+          });
+        } else {
+          resolve(value);
+        }
+      });
+    });
   };
 }
 
